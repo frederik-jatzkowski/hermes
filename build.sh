16dee3b01bad7e1f7a2cf9532c4434d612b1b5f0
Frederik Jatzkowski <f.l.jatzkowski@gmail.com>
2022-04-08 17:36:17 +0200

    improved build pipeline

--- build.sh
+++ build.sh
@@ -2,27 +2,31 @@
 
 # env GOOS=linux GOARCH=arm64 go build -o /run/hermes/hermes ./hermes.go
 
-# compile arm64
+# array of ressources
+res=(
+    "/opt/hermes/hermes"
+    "/etc/hermes/config.xml"
+    "/var/log/hermes/launch.log"
+    "/var/log/hermes/operation.log"
+    "/etc/systemd/system/hermes.service"
+    "/etc/letsencrypt/renewal-hooks/post/hermes_start.sh"
+    "/etc/letsencrypt/renewal-hooks/pre/hermes_stop.sh"
+)
+arch=amd64 # build arm64
 cd src
-if env GOOS=linux GOARCH=amd64 go build -o ../dist/opt/hermes/hermes ./hermes.go ; then
+if env GOOS=linux GOARCH=$arch go build -o ../dist/opt/hermes/hermes ./hermes.go ; then
     cd ../dist
-    # distribute ressources to absolute paths
-    sudo cp -R ./etc /
-    sudo cp -R ./opt /
-    sudo cp -R ./var /
-    # array of ressources
-    res=(
-        "/opt/hermes/hermes"
-        "/etc/hermes/config.xml"
-        "/var/log/hermes/launch.log"
-        "/var/log/hermes/operation.log"
-        "/etc/systemd/system/hermes.service"
-        "/etc/letsencrypt/renewal-hooks/post/hermes_start.sh"
-        "/etc/letsencrypt/renewal-hooks/pre/hermes_stop.sh"
-    )
-    # archive all ressources
-    sudo tar cvzfP ./hermes_linux_amd64.tar.gz ${res[*]}
-    # cleanup of the ressources on absolute paths
-    sudo rm -R ${res[*]}
+    sudo cp -R ./etc ./opt ./var / # distribute ressources to absolute paths
+    sudo tar cvzfP ./hermes_linux_$arch.tar.gz ${res[*]} # archive all ressources
+    sudo rm -R ${res[*]} # cleanup of the ressources on absolute paths
+fi
+cd ..
+arch=arm64 # build arm64
+cd src
+if env GOOS=linux GOARCH=$arch go build -o ../dist/opt/hermes/hermes ./hermes.go ; then
+    cd ../dist
+    sudo cp -R ./etc ./opt ./var / # distribute ressources to absolute paths
+    sudo tar cvzfP ./hermes_linux_$arch.tar.gz ${res[*]} # archive all ressources
+    sudo rm -R ${res[*]} # cleanup of the ressources on absolute paths
 fi
 cd ..
